cc64b4f12211e9846f85fff53450c481d5f7aa4a
Joe <ashzhouyue@gmail.com>
2017-08-01 12:52:38 +0930

    [TWEAK] Nested object sortable test added

--- spec/javascripts/sortable_default_config_spec.js
+++ spec/javascripts/sortable_default_config_spec.js
@@ -8,17 +8,17 @@ describe("default config test", function() {
     $('#test-part').sortableLabel({
       fieldName: "test_field"
     });
-    var allElements = $('.fields');
+    var allElements = $('#test-part .fields');
 
     $(allElements[2]).insertBefore($(allElements[0])).trigger('sortableLabel:refresh');
 
-    $('label').each(function(index, item) {
+    $('#test-part label').each(function(index, item) {
     	expect($(item).html()).toBe('Step '+String(index+1));
     });
 
     var expectedIdList = '3124',
     		targetIdList = '';
-    $('.fields').each(function(inedx, item) {
+    $('#test-part .fields').each(function(inedx, item) {
     	targetIdList += String($(item).attr('id'));
     });
     expect(targetIdList).toBe(expectedIdList);
@@ -30,43 +30,42 @@ describe("default config test", function() {
   	$('#test-part').sortableLabel({
   		label: "Day"
   	});  
-    var allElements = $('.fields.ui-sortable-handle');
+    var allElements = $('#test-part .fields.ui-sortable-handle');
   	$(allElements[2]).insertBefore($(allElements[0])).trigger('sortableLabel:refresh');
-  	$('label').each(function(index, item) {
+  	$('#test-part label').each(function(index, item) {
     	expect($(item).html()).toBe(weekday[index]);
     });
     var expectedIdList = '3124',
     		targetIdList = '';
-    $('.fields').each(function(inedx, item) {
+    $('#test-part .fields').each(function(inedx, item) {
     	targetIdList += String($(item).attr('id'));
     });
   });
 
   it("has deleted label", function() {
-  	$('.fields').last().append("<div class='fields' id='5'><label></label><a class='remove_nested_fields' data-association='test_field'></a><input id='5_destroy' value='false'><input class='position-field'></div>");
+  	$('#test-part .fields').last().append("<div class='fields' id='5'><label></label><a class='remove_nested_fields' data-association='test_field'></a><input id='5_destroy' value='false'><input class='position-field'></div>");
 
   	$('#test-part').sortableLabel({
       fieldName: "test_field"
     });
 
-    $('#3_destroy').val('1');
+    $('#test-part #3_destroy').val('1');
   	$($('.fields')[3]).css('display', 'none');
 
   	$('#test-part').trigger('sortableLabel:refresh');
 
-  	$('label:visible').each(function(index, item) {
-      console.log($(item).html());
+  	$('#test-part label:visible').each(function(index, item) {
   		expect($(item).html()).toBe('Step '+String(index+1));
   	});
     var expectedIdList = '1245',
     		targetIdList = '';
-    $('.fields:visible').each(function(inedx, item) {
+    $('#test-part .fields:visible').each(function(inedx, item) {
     	targetIdList += String($(item).attr('id'));
     });
   });
 
   it("nested objects", function() {
-    $('#test-part2').sortbleLabel({
+    $('#test-part2').sortableLabel({
       fieldName: 'test_field2',
       labelTarget: 'label[rel=2]',
       nestedTarget: function() {
@@ -78,12 +77,25 @@ describe("default config test", function() {
     });
 
     var allElements = $('.test-part3 .fields');
-    $(allElements[2]).insertBefore($(allElements[0]));
+    $(allElements[2]).insertBefore($(allElements[0])).trigger('sortableLabel:refresh');
 
-    $('#test-part2')..trigger('sortableLabel:refresh');
+    var expectedParentId = '1234',
+        targetParentId = '';
 
-    console.log($('#test-part2').html());
+    $('#test-part2 > .fields').each(function(index, item) {
+      targetParentId += $(item).attr('id');
+    });
 
+    expect(targetParentId).toBe(expectedParentId);
+
+     var expectedNestedId = '31241234',
+        targetNestedId = '';
+
+    $('.test-part3 > .fields').each(function(index, item) {
+      targetNestedId += $(item).attr('id');
+    });
+
+    expect(targetNestedId).toBe(expectedNestedId);   
 
   });
 });
